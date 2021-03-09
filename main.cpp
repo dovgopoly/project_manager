6366bc9a068a2c20a873c1f318df809d6b6a02e7
dovgopoly <idovgopoly@gmail.com>
2021-03-09 04:29:27 +0200

    Do changes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <QApplication>
 
 #include "includes/main_window.hpp"
-#include "includes/ticket_edit_window.hpp"
-
-/*
- * TODO:
- *   3. Sign out button like in an example
- *   4. Shadows
- *   6. Try to do animation
- *   7. Splitter in ticket window which displays ticket's description
- *   8. Add waiting spinner
- */
 
 int main(int argv, char **args) {
     QApplication application(argv, args);
